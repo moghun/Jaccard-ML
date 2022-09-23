f913347713c20bfee51853a064c236293bcff1ae
AmroAlJundi <amr.jundi@gmail.com>
2022-09-23 17:39:52 +0000

    Added explicit instantiations to JSONWrapper in json.cc

--- src/json.cc
+++ src/json.cc
@@ -5,6 +5,56 @@
 #include <unordered_map>
 #include <mutex>
 
+JSONWrapper JSONWrapper::GetJSON(std::string key){
+    JSONWrapper newone;
+    newone.object = object[key];
+    return newone;
+}
+void JSONWrapper::SetJSON(std::string key, JSONWrapper value){
+    object[key] = value.object;
+}
+void JSONWrapper::SetJSONNested(std::string key1, std::string key2, JSONWrapper value){
+    object[key1][key2] = value.object;
+}
+void JSONWrapper::SetJSONNested(std::string key1, std::string key2, std::string key3, JSONWrapper value){
+    object[key1][key2][key3] = value.object;
+}
+template <typename Value>
+void JSONWrapper::Set(std::string key, Value value){
+    object[key] = value;
+}
+template <typename Value>
+void JSONWrapper::NestedSet(std::string key1, std::string key2,Value value){
+    object[key1][key2] = value;
+}
+template <typename Value>
+void JSONWrapper::NestedSet(std::string key1, std::string key2,std::string key3,Value value){
+    object[key1][key2][key3] = value;
+}
+template <typename Value>
+void JSONWrapper::NestedSet(std::string key1, std::string key2, std::string key3, std::string key4, Value value){
+    object[key1][key2][key3][key4] = value;
+}
+template <typename Value>
+Value JSONWrapper::Get(std::string key){
+    return object[key];
+}
+template <typename Value>
+Value JSONWrapper::NestedGet(std::string key, std::string key2){
+    return object[key][key2];
+}
+template <typename Value>
+Value JSONWrapper::NestedGet(std::string key, std::string key2, std::string key3){
+    return object[key][key2][key3];
+}
+template <typename Value>
+Value JSONWrapper::NestedGet(std::string key, std::string key2, std::string key3, std::string key4){
+    return object[key][key2][key3][key4];
+}
+bool JSONWrapper::contains(std::string key){
+    return object.contains(key);
+}
+#include "json.inc"
 class JSONStore{
 private:
     static std::unordered_map<int, nlohmann::json> store_;
--- src/json.h
+++ src/json.h
@@ -5,58 +5,31 @@
 #define _JSON_WRAPPER
 
 class JSONWrapper {
+
     nlohmann::json object;
 
 public:
-    JSONWrapper GetJSON(std::string key){
-        JSONWrapper newone;
-        newone.object = object[key];
-        return newone;
-    }
-    void SetJSON(std::string key, JSONWrapper value){
-        object[key] = value.object;
-    }
-    void SetJSONNested(std::string key1, std::string key2, JSONWrapper value){
-        object[key1][key2] = value.object;
-    }
-    void SetJSONNested(std::string key1, std::string key2, std::string key3, JSONWrapper value){
-        object[key1][key2][key3] = value.object;
-    }
+    JSONWrapper GetJSON(std::string key);
+    void SetJSON(std::string key, JSONWrapper value);
+    void SetJSONNested(std::string key1, std::string key2, JSONWrapper value);
+    void SetJSONNested(std::string key1, std::string key2, std::string key3, JSONWrapper value);
     template <typename Value>
-    void Set(std::string key, Value value){
-        object[key] = value;
-    }
+    void Set(std::string key, Value value);
     template <typename Value>
-    void NestedSet(std::string key1, std::string key2,Value value){
-        object[key1][key2] = value;
-    }
+    void NestedSet(std::string key1, std::string key2,Value value);
     template <typename Value>
-    void NestedSet(std::string key1, std::string key2,std::string key3,Value value){
-        object[key1][key2][key3] = value;
-    }
+    void NestedSet(std::string key1, std::string key2,std::string key3,Value value);
     template <typename Value>
-    void NestedSet(std::string key1, std::string key2, std::string key3, std::string key4, Value value){
-        object[key1][key2][key3][key4] = value;
-    }
+    void NestedSet(std::string key1, std::string key2, std::string key3, std::string key4, Value value);
     template <typename Value>
-    Value Get(std::string key){
-        return object[key];
-    }
+    Value Get(std::string key);
     template <typename Value>
-    Value NestedGet(std::string key, std::string key2){
-        return object[key][key2];
-    }
+    Value NestedGet(std::string key, std::string key2);
     template <typename Value>
-    Value NestedGet(std::string key, std::string key2, std::string key3){
-        return object[key][key2][key3];
-    }
+    Value NestedGet(std::string key, std::string key2, std::string key3);
     template <typename Value>
-    Value NestedGet(std::string key, std::string key2, std::string key3, std::string key4){
-        return object[key][key2][key3][key4];
-    }
-    bool contains(std::string key){
-        return object.contains(key);
-    }
+    Value NestedGet(std::string key, std::string key2, std::string key3, std::string key4);
+    bool contains(std::string key);
 
     friend std::istream& operator>>(std::istream& in, JSONWrapper& j);
 
